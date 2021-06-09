235751ba7d0208ad360f9cd4cf94566b0cbbf268
oybiodoo <61978207+oybiodoo@users.noreply.github.com>
2021-06-09 08:40:39 -0400

    Add files via upload

--- open_cashbox/models/models.py
+++ open_cashbox/models/models.py
@@ -5,3 +5,7 @@ from odoo import models, fields, api
 
 class PosSessionInherited(models.Model):
     _inherit = 'pos.session'
+
+    def action_open_cash_box(self):
+        for rec in self:
+            rec.cash_register_id.open_cashbox_id()
